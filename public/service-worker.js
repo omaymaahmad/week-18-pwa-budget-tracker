487863c2ad435d0a90e7859cb01439f1c8ce3e6d
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-17 18:32:10 +0000

    feat: set up cache variables and urls

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+// create the variables that name the cache 
+var CACHE = "budget-cache";
+var DATA_CACHE = "budget-data-cache";
+
+// create an array which stores the urls that we want to save in cache
+var applicationUrls = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/styles.css",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+]
