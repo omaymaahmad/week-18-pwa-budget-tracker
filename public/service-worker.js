e02d19646c8c29adc69b36070d3a457308a3d12b
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-19 14:27:41 +0000

    Added fetch functionality service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,12 +16,40 @@ var applicationUrls = [
 self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open(CACHE).then(function(cache) {
+            console.log("Cache working")
             return cache.addAll(applicationUrls)
         })
     )
 });
 
 self.addEventListener("fetch", function(event){
-    
+if(event.request.url.includes("/api/")){
+    event.respondWith(
+        caches.open(DATA_CACHE).then((cache) => {
+            return fetch(event.request).then((response) => {
+                if(response.status === 200){
+                    cache.put(event.request.url, response())
+                }
+                return response;
+            }).catch((err) => {
+                return cache.match(event.request)
+            })
+        }).catch((err) => console.log(err))
+    ).catch; return; 
+}
+
+event.respondWith(
+    fetch(event.request).catch(function(){
+        return caches.match(event.request).then((response)=>{
+            if(response){
+                return response;
+            } else if (event.request.headers.get('accept').includes("text/html")){
+                return caches.match("/")
+            }
+        });
+    })
+)
+
+
 
 })
