42db54f3bed97c7b3cabd9c6811cdcf2595f28ef
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-19 13:20:27 +0000

    feat: get request to get all transactions in db

--- public/db.js
+++ public/db.js
@@ -1,6 +1,14 @@
 // this file is all about setitng up indexedDb 
+const router = require("express").Router();
+const Transaction = require("../models/transaction");
 
-// in this file we will have an event listener on the window itself waiting for it to come online...
+// get all entries
+router.get("/api/transactoin", (req, res) => {
+    Transaction.find({}).then(dbTransaction => {
+        res.json(dbTransaction);
+    })
+    .catch(err => {
+        res.status(404).json(err);
+    });
+});
 
-// when the app comes online - we need to check the database, get each entry in the db..
-// then send them all to the /api/transaction/bulk and point
